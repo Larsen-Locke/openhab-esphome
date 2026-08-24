c91b39176c4bac86725c64e4013ac9504d864058
Carsten Mogge <carsten.mogge@gmail.com>
2026-08-24 20:19:49 +0500

    Aktualisieren von ESPHomeHandler.java

--- src/main/java/no/seime/openhab/binding/esphome/internal/handler/ESPHomeHandler.java
+++ src/main/java/no/seime/openhab/binding/esphome/internal/handler/ESPHomeHandler.java
@@ -206,11 +206,13 @@ public class ESPHomeHandler extends BaseThingHandler implements CommunicationLis
         versionService.addListener(this);
 
         if (config.deepSleep) {
-           if (config.deepSleepAssumeOnline) {
-updateStatus(ThingStatus.ONLINE)
-           else {
+            if (config.deepSleepAssumeOnline) {
+                updateStatus(ThingStatus.ONLINE)
+            }
+            else {
                 updateStatus( ThingStatus.UNKNOWN, ThingStatusDetail.NONE, "Waiting for device to wake up from deep sleep");
-        } 
+            } 
+        }
         else {
             exponentialBackoff = new ExponentialBackoff(config.reconnectInterval, config.maxReconnectInterval);
             if (config.hostname != null && !config.hostname.isEmpty()) {
